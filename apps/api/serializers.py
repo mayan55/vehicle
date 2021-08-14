9ea1b1bc04f72f7b7c8e08c7237ac12b418b5100
muhammet_ayan <you@example.com>
2021-08-14 23:34:09 +0300

    completed

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -16,18 +16,5 @@ class VehicleSerializers(serializers.ModelSerializer):
 
     vehicle_model = VehicleModelSerializers(many=False, read_only=True)
     vehicle_model_id = serializers.IntegerField(required=True, write_only=True)
-    # id = serializers.IntegerField(read_only=True)
-    # plaka = serializers.CharField(required=True, max_length=12)
-    # km = serializers.IntegerField(required=True)
-    # sase_no = serializers.CharField(required=True, max_length=100)
-    # renk = serializers.CharField(required=True, max_length=24)
 
-    # def create(self, validated_data):
-    #     return Vehicle.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     for field, value in validated_data.items():
-    #         setattr(instance, field, value)
-    #     instance.save()
-    #     return instance
 
